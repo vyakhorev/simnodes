b35fd2376974e8d0e594f7810987e3201382f5c4
vyakhorev <alexey.vyakhorev@gmail.com>
2015-12-22 16:18:16 +0300

    testing

--- main_test_sim.py
+++ main_test_sim.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     consumer.set_cons_conv('food', 10, 1)
     consumer.spawn_item('money', 100)
     consumer.spawn_item('food', 10)
+    print('Hello')
 
     producer = cItemConsumer('food factory')
     producer.set_prod_conv('food', 100, 5)
