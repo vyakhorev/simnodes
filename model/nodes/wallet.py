66ac6424808cf65c90ed08d5d1bc2d1a62e9763d
iluadd <ildozh@gmail.com>
2015-12-23 15:57:06 +0300

    New version of wallet, with working get() and take() functions

--- model/nodes/wallet.py
+++ model/nodes/wallet.py
@@ -7,40 +7,48 @@ from collections import namedtuple
 
 class cValuedResource(simulengin.cConnToDEVS):
 
-    def __init__(self, name, value=0):
+    def __init__(self, name):
         super().__init__()
         self.name = name
-        # self.value = value
-        if self.is_simulation_running:
-            self.value = simpy.Container(self.simpy_env)
-        else:
-            self.value = mtPile(level=value)
-
-    # def __add__(self, other):
-    #     return self.value + other.value
-    #
-    # def __sub__(self, other):
-    #     return self.value - other.value
-
-    def set_capacity(self, value):
-        self.value.add(value)
 
     def my_generator(self):
         yield self.empty_event()
 
     def __repr__(self):
-        return str(self.name) + " : " + str(self.value)
+        return str(self.name)
 
-class cItem(cValuedResource): #store
-    pass
 
-class cPile(cValuedResource): #container
-    pass
+class cItem(cValuedResource):
+    def __init__(self, name, count=0):
+        super().__init__(name)
+
+        if self.is_simulation_running:
+            self.count = simpy.Store(self.simpy_env)
+        else:
+            self.count = mtQueue(count)
+
+    def __repr__(self):
+        return str(self.name) + " : " + str(self.count.items)
+
+
+class cPile(cValuedResource):
+    def __init__(self, name, value=0):
+        super().__init__(name)
+
+        if self.is_simulation_running:
+            self.value = simpy.Container(self.simpy_env)
+        else:
+            self.value = mtPile(level=value)
+
+    def __repr__(self):
+        return str(self.name) + " : " + str(self.value.level)
+
 
 class cWallet(simulengin.cConnToDEVS):
     # Todo: different types of resources
 
     def __init__(self):
+        super().__init__()
         self.res_all = {}
         self.my_pile = mtPile(level=100)
 
@@ -68,11 +76,11 @@ class cWallet(simulengin.cConnToDEVS):
             res_i.init_sim()
 
     def my_generator(self):
-        self.sent_log("test - taking 10")
-        yield self.my_pile.get(10)
-        print(self.my_pile.level)
-        self.sent_log("test - took 10 !!! :-)")
-        self.as_process(self.gen_do_conversion('rubles', 40, 'dollars', 0.8))
+        # self.sent_log("test - taking 10")
+        # yield self.my_pile.get(10)
+        # print(self.my_pile.level)
+        # self.sent_log("test - took 10 !!! :-)")
+        # self.as_process(self.gen_do_conversion('rubles', 40, 'dollars', 0.8))
         yield self.empty_event()
 
     def check_qtty(self, name):
@@ -81,26 +89,39 @@ class cWallet(simulengin.cConnToDEVS):
         qtty = self.res_all[name].value
         return qtty
 
-    def add_items(self, name, qtty):
+    def gen_add_items(self, name, qtty):
         if not self.check_existance(name):
             self.spawn_item(name, 0)
             # This was a workaround before self.is_simulation_running
             self.res_all[name].s_set_devs(self.devs)
-        self.res_all[name].value.put(qtty)
+        if type(self.res_all[name]) == cPile:
+            self.res_all[name].level.put(qtty)
+        if type(self.res_all[name]) == cItem:
+            self.res_all[name].count.put(qtty)
+        print('BLAH BLAH')
         yield self.empty_event()
 
-    def take_qtty(self, name, qtty):
-        print("*"*20)
-        self.sent_log(self.res_all[name].value.level)
-        print("*"*20)
-        #qtty_available = self.check_qtty(name)
-        #self.sent_log('qtty_available {}'.format(qtty_available.level))
-        if self.res_all[name].value.level < qtty:
-            self.sent_log("not enough items")
-            # Alternative path?
-        return self.res_all[name].value.get(qtty)
+    def gen_take_qtty(self, name, qtty):
+        # if name not in self.res_all:
+        #     self.sent_log('{} has not {} key'.format(type(self), name))
+        #     return False
 
-    # /?
+        if type(self.res_all[name]) == cPile:
+            print('value {} '.format(self.res_all[name].value.level))
+
+            if self.res_all[name].value.level < qtty:
+                self.sent_log("not enough value level ")
+                # Alternative path?
+            yield self.res_all[name].value.get(qtty)
+
+        elif type(self.res_all[name]) == cItem:
+            print('count {} '.format(self.res_all[name]))
+            if len(self.res_all[name].count.items()) <= 0:
+                self.sent_log("not enough items")
+            yield self.res_all[name].value.get(qtty)
+
+        yield self.empty_event()
+
 
     # *** INTERFACE
 
@@ -111,23 +132,33 @@ class cWallet(simulengin.cConnToDEVS):
     # TODO: for multiple items to multiple items
     # TODO: yield "gen_do"
     # TODO: apply worker for conversion?
+    # TODO: rejected logic
 
-    def gen_do_exchange(self, resource_from, qtty_from, resource_to, qtty_to):
-        yield self.take_qtty(resource_from, qtty_from)
-        self.sent_log('item taken!')
-        self.as_process(self.add_items(resource_to, qtty_to))
-        self.sent_log('item added!')
-        yield self.empty_event()
+    def gen_wallet_take(self, resource, qtty):
+        yield self.simpy_env.process(self.gen_take_qtty(resource, qtty))
+        self.sent_log('item taken !')
 
-    def gen_do_conversion(self, resource_from, qtty_from, resource_to, qtty_to):
-        yield self.timeout(40)
-        self.as_process(self.gen_do_exchange(resource_from, qtty_from, resource_to, qtty_to))
+    def gen_wallet_add(self, resource, qtty):
+        yield self.simpy_env.process(self.gen_add_items(resource, qtty))
+        self.sent_log('item added !')
 
-    def gen_do_multiple_exchange(self):
-        pass
 
-    def gen_do_multiple_conversion(self):
-        pass
+    # def gen_do_exchange(self, resource_from, qtty_from, resource_to, qtty_to):
+    #     yield self.take_qtty(resource_from, qtty_from)
+    #     self.sent_log('item taken!')
+    #     self.as_process(self.add_items(resource_to, qtty_to))
+    #     self.sent_log('item added!')
+    #     yield self.empty_event()
+    #
+    # def gen_do_conversion(self, resource_from, qtty_from, resource_to, qtty_to):
+    #     yield self.timeout(40)
+    #     self.as_process(self.gen_do_exchange(resource_from, qtty_from, resource_to, qtty_to))
+    #
+    # def gen_do_multiple_exchange(self):
+    #     pass
+    #
+    # def gen_do_multiple_conversion(self):
+    #     pass
 
     def __repr__(self):
         return str(self.res_all)
@@ -152,15 +183,41 @@ class cDeal:
 class cDealWallet:
     def create_deal(self):
         # simpy resource that would be used in the Node
-
         pass
-    #???
 
-    # def do_instance_exchange(self, resource_in, resource_out):
-    #     if self.check_existance(resource_in) or not(self.check_existance(resource_out)):
-    #         return False
-    #
-    #     return True
+
+class nodochka(simulengin.cConnToDEVS):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+
+    def my_generator(self):
+        self.as_process(self.gen_debug())
+        self.as_process(self.some_gen('Take', self.a, 'rubles', 150))
+        self.as_process(self.some_gen('Add', self.b, 'gold ingots', 27))
+        yield self.empty_event()
+
+    def gen_debug(self):
+        while True:
+            print(self.a)
+            print(self.b)
+            yield self.timeout(2)
+
+    def some_gen(self, operation, patient, resource, qtty):
+        yield self.simpy_env.timeout(40)
+        if operation == 'Take':
+            self.as_process(patient.gen_wallet_take(resource, qtty))
+            print('taker')
+            yield self.simpy_env.timeout(1)
+        elif operation == 'Add':
+            print('adder')
+            self.as_process(patient.gen_wallet_add(resource, qtty))
+            yield self.simpy_env.timeout(1)
+        else:
+            print('none operation')
+            yield self.simpy_env.timeout(1)
+
 
 if __name__ == '__main__':
     import datetime
@@ -168,13 +225,20 @@ if __name__ == '__main__':
     the_model = cNodeFieldModel()
 
     a = cWallet()
-    a.spawn_item('rubles', 100)
-    # b = cWallet()
-    # b.spawn_item('dollars', 1.5)
+    a.spawn_pile('rubles', 100)
+    b = cWallet()
+    b.spawn_item('gold ingots', 4)
+
+    nod1 = nodochka(a, b)
+
+    # test_env.process(some_gen('Take', a, 'rubles', 7, test_env))
+    # test_env.process(some_gen('Add', b, 'gold ingots', 27, test_env))
 
     # print('a : {}, b : {}'.format(a, b))
     the_model.addOtherSimObj(a)
-    # the_model.addOtherSimObj(b)
+    the_model.addOtherSimObj(b)
+    the_model.addOtherSimObj(nod1)
+
     loganddata, runner = the_model.run_sim(datetime.date(2015, 11, 15), until=100, seed=555)
     # a.gen_do_conversion('rubles', 40, 'dollar', 1)
     # print(a.res_slice(cItem))
