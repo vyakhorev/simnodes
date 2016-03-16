1ca111f20e9295b5725af358214c64e662feee95
vyakhorev <Alexey.Vyakhorev@gmail.com>
2016-03-16 17:25:37 +0300

    BlueNodes iteration... Nothing works at the moment.

--- model/nodes/classes/BlueNodes.py
+++ model/nodes/classes/BlueNodes.py
@@ -7,7 +7,9 @@ import model.nodes.simulengin.simulengin as simulengin
 from model.nodes.wallet import cWallet
 
 class cBaseBlueAgent(cNodeBase, cSimNode):
-    # Нода с входным и выходным портом
+    """
+    Нода с одним входом 1:1 и одним выходом 1:1
+    """
 
     def __init__(self, name):
         super().__init__(name)
@@ -29,15 +31,52 @@ class cBaseBlueAgent(cNodeBase, cSimNode):
     def register_daemon(self, new_daemon):
         self.daemons += [new_daemon]
 
-    def connect_input_node(self, input_node):
-        # в боевой версии, конечно, надо сделать подключение по порту, а не по ноде
-        self.in_orders.connect_to_port(input_node.out_orders)
+    def connect_to_input(self, port_to_connect):
+        self.in_orders.connect_to_port(port_to_connect)
 
-    def connect_output_node(self, output_node):
-        self.out_orders.connect_to_port(output_node.in_orders)
+    def connect_to_output(self, port_to_connect):
+        self.out_orders.connect_to_port(port_to_connect)
+
+class cNodeTradeContract(cNodeBase, cSimNode):
+    """
+    Нода, взаимодействующая с двумя другими по поводу купли-продажи
+    Демон в ней обеспечивает выполнение паттерна "контракт" для
+    каждого запроса.
+    """
+    def __init__(self, name):
+        super().__init__(name)
+
+        self.out_party_buyer = ports.cOnetoOneOutQueue(self)
+        self.in_party_buyer = ports.cOnetoOneInpQueue(self)
+        self.out_party_seller = ports.cOnetoOneOutQueue(self)
+        self.in_party_seller = ports.cOnetoOneInpQueue(self)
+        self.ports = [self.out_party_buyer, self.in_party_buyer, self.out_party_seller, self.in_party_seller]
+
+        for p_i in self.ports:
+            self.register_port(p_i)
+
+        self.contracts_demon = cContractListDaemon()
+        self.daemons = []
+
+    def connect_inout_buyer(self, in_port, out_port):
+        # Этот метод для тестирования. По-хорошему, должна быть гибкость,
+        # куда коннектить in порт и out порт. Так что один порт сделать низя.
+        self.out_party_buyer.connect_to_port(in_port)
+        self.in_party_buyer.connect_to_port(out_port)
+
+    def connect_inout_seller(self, in_port, out_port):
+        # Этот метод для тестирования. По-хорошему, должна быть гибкость,
+        # куда коннектить in порт и out порт. Так что один порт сделать низя.
+        self.out_party_seller.connect_to_port(in_port)
+        self.in_party_seller.connect_to_port(out_port)
+
+    def my_generator(self):
 
 
-class cAgentTrader(cBaseBlueAgent):
+    def gen_(self):
+
+
+class cNodeAgentTrader(cBaseBlueAgent):
     """
     1:1 input, 1:1 output
     И покупатель, и продавец (зависимо от настройки)
@@ -61,10 +100,9 @@ class cAgentTrader(cBaseBlueAgent):
         # Демон, готовый обрабатывать запросы на закупку
         # (открывать договор на поставку)
 
-    def init_contractor(self):
-        # создаем демона, отвечающего за менеджмент контрактов
-
     def my_generator(self):
+
+
 
 
 class cContractListDaemon(simulengin.cConnToDEVS):
@@ -109,10 +147,19 @@ class cTask:
     def __init__(self):
         # Чтобы задать таск нужно задать текущий стейт.
         # Из него уже можно всё дерево вытащить.
+        self.previous_state = None
         self.active_state = None
+        self.expanded_states = [] # Пополняется по мере выполнения
 
-    def add_state(self, name, transition):
+    def set_state(self, start_state):
+        # Все последующие переходы заданы в стейте
+        self.active_state = start_state
 
+    def peek_next_states(self):
+        # Для выбора следующего стейта нода может получить
+        # следующие стейты и переходы.
+        for trans_i, state_i in self.active_state.peek_next_states():
+            yield trans_i, state_i
 
 
 class cTaskTransition:
@@ -126,6 +173,12 @@ class cTaskTransition:
 
     def add_req(self, a_req):
         self.requerments += [a_req]
+
+    def get_start(self):
+        return self.state0
+
+    def get_end(self):
+        return self.state1
 
 class cTaskState:
     """
@@ -146,6 +199,10 @@ class cTaskState:
             next_transition.add_req(req_i)
         # write it down
         self.transitions += [next_transition]
+
+    def peek_next_states(self):
+        for trans in self.transitions:
+            yield (trans, trans.get_end())
 
     # get event
     def subscribe_start(self):
