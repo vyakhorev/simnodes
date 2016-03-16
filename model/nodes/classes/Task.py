79a23a832a23a3f5198ae826d61b17cdf8a3d076
iluadd <ildozh@gmail.com>
2016-03-16 10:49:57 +0300

    Revert "Revert "Testing Market system""
    This reverts commit 9ad0593cace8560f39d5a79cb30c7f6110683213.

--- model/nodes/classes/Task.py
+++ model/nodes/classes/Task.py
@@ -167,4 +167,8 @@ class cDelivery(cTask):
         super().__init__(name, **kwargs)
         self.urgent = urgent
 
+class cMarketTask(cTask):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
 
